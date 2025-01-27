5e95156e14349e30d4e95f2f519ac877ecb37020
Adrian Murawski <adrianmurawski79@gmail.com>
2025-01-27 19:25:21 +0100

    added plot methods

--- src/generate_plots.py
+++ src/generate_plots.py
@@ -1,74 +1,260 @@
 import matplotlib.pyplot as plt
 import json
-import os
+import seaborn as sns
 
 
-def plot_avg_fitness_per_iteration_logscale(results_df, output_dir="plots/fit"):
-    os.makedirs(output_dir, exist_ok=True)
-    for (F, CR, dim) in results_df.groupby(['F', 'CR', 'dim']).groups:
-        plt.figure(figsize=(12, 8))
-        min_fitness_annotations = []
-        for mode in ['normal', 'PSR', 'MSR']:
-            subset = results_df[(results_df['F'] == F) & 
-                                (results_df['CR'] == CR) & 
-                                (results_df['dim'] == dim) & 
-                                (results_df['mode'] == mode)]
+def plot_demonstration(results_df):
+    sns.set_style("whitegrid")
+    functions = results_df['function'].unique()
+    modes = results_df['mode'].unique()
+    dims = results_df['dim'].unique()
+    fs = results_df['F'].unique()
+    
+    for func in functions:
+        for dim in dims:
+            for F in fs:
+                fig, axes = plt.subplots(len(modes), 3, figsize=(18, 4 * len(modes)))
 
-            min_fitness = float('inf')
-            for _, row in subset.iterrows():
-                avg_fitnesses = json.loads(row['avg_fitnesses'])
-                plt.plot(range(1, len(avg_fitnesses) + 1), avg_fitnesses, label=f"{mode}")
-                min_fitness = min(min_fitness, min(avg_fitnesses))
+                for i, mode in enumerate(modes):
+                    subset = results_df[(results_df['function'] == func) & (results_df['mode'] == mode) & (results_df['dim'] == dim) & (results_df['F'] == F)]
 
-            min_fitness_annotations.append((mode, min_fitness))
+                    if subset.empty:
+                        continue
 
-        annotation_text = "\n".join([f"{mode}: min fitness = {min_fitness:.2e}" for mode, min_fitness in min_fitness_annotations])
-        plt.text(0.05, 0.95, annotation_text, transform=plt.gca().transAxes, fontsize=10,
-                verticalalignment='top', bbox=dict(boxstyle="round,pad=0.3", edgecolor='gray', facecolor='lightyellow'))
+                    avg_fitnesses = json.loads(subset.iloc[0]['avg_fitnesses'])
+                    f_trace_value = subset.iloc[0]['f_trace']
+                    if isinstance(f_trace_value, str):  
+                        f_trace = json.loads(f_trace_value)  
+                    else:  
+                        f_trace = [f_trace_value]
 
-        plt.title(f"Avg Fitness per Iteration (F={F}, CR={CR}, dim={dim}, Log Scale)")
-        plt.xlabel("Iteration")
-        plt.ylabel("Average Fitness (Log Scale)")
-        plt.yscale('log')
-        plt.legend(loc='best', fontsize='small')
-        plt.grid(True, which="both", linestyle='--', linewidth=0.5)
+                    result_vars_value = subset.iloc[0]['result_vars']
+                    if isinstance(result_vars_value, str):  
+                        result_vars = json.loads(result_vars_value)  
+                    else:  
+                        result_vars = [result_vars_value]
 
-        filename = f"F_{F}_CR_{CR}_dim_{dim}_logscale.png"
-        plt.savefig(os.path.join(output_dir, filename))
-        plt.close()
+                    dim = subset.iloc[0]['dim']
+                    filename = f"../plots/demonstration/{func}_{dim}_{F}.png"
+
+                    ax1 = axes[i, 0]
+                    ax1.plot(range(len(avg_fitnesses)), avg_fitnesses, label=f"{mode} - avg fitness")
+                    ax1.set_yscale('log')
+                    ax1.set_title(f"{func} - {mode} (avg fitness)")
+                    ax1.set_xlabel("Iteration")
+                    ax1.set_ylabel("Avg Fitness (log scale)")
+                    ax1.legend()
+
+                    min_fitness = min(avg_fitnesses)
+                    min_fitness_index = avg_fitnesses.index(min_fitness)
+
+                    ax1.text(min_fitness_index, min_fitness, f"Min: {min_fitness:.2e}", color='blue', fontsize=10, ha='center', va='bottom')
+
+                    ax2 = axes[i, 1]
+                    ax2.plot(range(len(f_trace)), f_trace, label=f"{mode} - f_trace", color='r')
+                    ax2.set_title(f"{func} - {mode} (f_trace)")
+                    ax2.set_xlabel("Iteration")
+                    ax2.set_ylabel("F")
+                    ax2.legend()
+
+                    ax3 = axes[i, 2]
+                    ax3.plot(range(len(result_vars)), result_vars, label=f"{mode} - variance", color='g')
+                    ax3.set_title(f"{func} - {mode} (variance)")
+                    ax3.set_xlabel("Iteration")
+                    ax3.set_ylabel("Variance")
+                    ax3.legend()
+
+                plt.tight_layout()
+                plt.savefig(filename)
 
 
-def plot_result_vars_per_iteration_logscale(results_df, output_dir="plots/var"):
-    os.makedirs(output_dir, exist_ok=True)
-    for (F, CR, dim) in results_df.groupby(['F', 'CR', 'dim']).groups:
-        plt.figure(figsize=(12, 8))
-        
-        min_variance_annotations = []
-        for mode in ['normal', 'PSR', 'MSR']:
-            subset = results_df[(results_df['F'] == F) & 
-                                (results_df['CR'] == CR) & 
-                                (results_df['dim'] == dim) & 
-                                (results_df['mode'] == mode)]
+def plot_amplifier_reductor(results_df):
+    sns.set_style("whitegrid")
+    functions = results_df['function'].unique()
+    modes = results_df['mode'].unique()
+    dims = results_df['dim'].unique()
+    steps = results_df['step'].unique()
 
-            min_variance = float('inf')
-            for _, row in subset.iterrows():
-                result_vars = json.loads(row['result_vars'])
-                plt.plot(range(1, len(result_vars) + 1), result_vars, label=f"{mode}")
-                min_variance = min(min_variance, min(result_vars))
-            
-            min_variance_annotations.append((mode, min_variance))
+    for func in functions:
+        for dim in dims:
+            for mode in modes:
+                fig, axes = plt.subplots(len(steps), 3, figsize=(18, 4 * len(steps)))
+                for i, step in enumerate(steps):
+                    subset = results_df[(results_df['function'] == func) & (results_df['mode'] == mode) & (results_df['dim'] == dim) & (results_df['step'] == step)]
 
-        annotation_text = "\n".join([f"{mode}: min variance = {min_variance:.2e}" for mode, min_variance in min_variance_annotations])
-        plt.text(0.05, 0.95, annotation_text, transform=plt.gca().transAxes, fontsize=10,
-                verticalalignment='top', bbox=dict(boxstyle="round,pad=0.3", edgecolor='gray', facecolor='lightyellow'))
+                    if subset.empty:
+                        continue
 
-        plt.title(f"Result Vars per Iteration (F={F}, CR={CR}, dim={dim}, Log Scale)")
-        plt.xlabel("Iteration")
-        plt.ylabel("Result Vars (Log Scale)")
-        plt.yscale('log')
-        plt.legend(loc='best', fontsize='small')
-        plt.grid(True, which="both", linestyle='--', linewidth=0.5)
+                    avg_fitnesses = json.loads(subset.iloc[0]['avg_fitnesses'])
+                    f_trace_value = subset.iloc[0]['f_trace']
+                    if isinstance(f_trace_value, str):  
+                        f_trace = json.loads(f_trace_value)  
+                    else:  
+                        f_trace = [f_trace_value]
 
-        filename = f"F_{F}_CR_{CR}_dim_{dim}_logscale_result_vars.png"
-        plt.savefig(os.path.join(output_dir, filename))
-        plt.close()
+                    result_vars_value = subset.iloc[0]['result_vars']
+                    if isinstance(result_vars_value, str):  
+                        result_vars = json.loads(result_vars_value)  
+                    else:  
+                        result_vars = [result_vars_value]
+
+                    dim = subset.iloc[0]['dim']
+                    filename = f"../plots/amp_red/{func}_{dim}_{mode}.png"
+
+                    ax1 = axes[i, 0]
+                    ax1.plot(range(len(avg_fitnesses)), avg_fitnesses, label=f"{mode} - avg fitness")
+                    ax1.set_yscale('log')
+                    ax1.set_title(f"{func} - {mode} (avg fitness) - step {step}")
+                    ax1.set_xlabel("Iteration")
+                    ax1.set_ylabel("Avg Fitness (log scale)")
+                    ax1.legend()
+
+                    min_fitness = min(avg_fitnesses)
+                    min_fitness_index = avg_fitnesses.index(min_fitness)
+
+                    ax1.text(min_fitness_index, min_fitness, f"Min: {min_fitness:.2e}", color='blue', fontsize=10, ha='center', va='bottom')
+
+                    ax2 = axes[i, 1]
+                    ax2.plot(range(len(f_trace)), f_trace, label=f"{mode} - f_trace", color='r')
+                    ax2.set_title(f"{func} - {mode} (f_trace)")
+                    ax2.set_xlabel("Iteration")
+                    ax2.set_ylabel("F")
+                    ax2.legend()
+
+                    ax3 = axes[i, 2]
+                    ax3.plot(range(len(result_vars)), result_vars, label=f"{mode} - variance", color='g')
+                    ax3.set_title(f"{func} - {mode} (variance)")
+                    ax3.set_xlabel("Iteration")
+                    ax3.set_ylabel("Variance")
+                    ax3.legend()
+
+                plt.tight_layout()
+                plt.savefig(filename)
+
+
+def plot_target_ratio(results_df):
+    sns.set_style("whitegrid")
+    functions = results_df['function'].unique()
+    modes = results_df['mode'].unique()
+    dims = results_df['dim'].unique()
+    target_ratios = results_df['target_ratio'].unique()
+
+    for func in functions:
+        for dim in dims:
+            for mode in modes:
+                fig, axes = plt.subplots(len(target_ratios), 3, figsize=(18, 4 * len(target_ratios)))
+                for i, target_ratio in enumerate(target_ratios):
+                    subset = results_df[(results_df['function'] == func) & (results_df['mode'] == mode) & (results_df['dim'] == dim) & (results_df['target_ratio'] == target_ratio)]
+
+                    if subset.empty:
+                        continue
+
+                    avg_fitnesses = json.loads(subset.iloc[0]['avg_fitnesses'])
+                    f_trace_value = subset.iloc[0]['f_trace']
+                    if isinstance(f_trace_value, str):  
+                        f_trace = json.loads(f_trace_value)  
+                    else:  
+                        f_trace = [f_trace_value]
+
+                    result_vars_value = subset.iloc[0]['result_vars']
+                    if isinstance(result_vars_value, str):  
+                        result_vars = json.loads(result_vars_value)  
+                    else:  
+                        result_vars = [result_vars_value]
+
+                    dim = subset.iloc[0]['dim']
+                    filename = f"../plots/target_ratio/{func}_{dim}_{mode}.png"
+
+                    ax1 = axes[i, 0]
+                    ax1.plot(range(len(avg_fitnesses)), avg_fitnesses, label=f"{mode} - avg fitness")
+                    ax1.set_yscale('log')
+                    ax1.set_title(f"{func} - {mode} (avg fitness) - target_ratio {target_ratio}")
+                    ax1.set_xlabel("Iteration")
+                    ax1.set_ylabel("Avg Fitness (log scale)")
+                    ax1.legend()
+
+                    min_fitness = min(avg_fitnesses)
+                    min_fitness_index = avg_fitnesses.index(min_fitness)
+
+                    ax1.text(min_fitness_index, min_fitness, f"Min: {min_fitness:.2e}", color='blue', fontsize=10, ha='center', va='bottom')
+
+                    ax2 = axes[i, 1]
+                    ax2.plot(range(len(f_trace)), f_trace, label=f"{mode} - f_trace", color='r')
+                    ax2.set_title(f"{func} - {mode} (f_trace)")
+                    ax2.set_xlabel("Iteration")
+                    ax2.set_ylabel("F")
+                    ax2.legend()
+
+                    ax3 = axes[i, 2]
+                    ax3.plot(range(len(result_vars)), result_vars, label=f"{mode} - variance", color='g')
+                    ax3.set_title(f"{func} - {mode} (variance)")
+                    ax3.set_xlabel("Iteration")
+                    ax3.set_ylabel("Variance")
+                    ax3.legend()
+
+                plt.tight_layout()
+                plt.savefig(filename)
+
+
+def plot_threshold(results_df):
+    sns.set_style("whitegrid")
+    functions = results_df['function'].unique()
+    modes = results_df['mode'].unique()
+    dims = results_df['dim'].unique()
+    thresholds = results_df['threshold'].unique()
+
+    for func in functions:
+        for dim in dims:
+            for mode in modes:
+                fig, axes = plt.subplots(len(thresholds), 3, figsize=(18, 4 * len(thresholds)))
+                for i, threshold in enumerate(thresholds):
+                    subset = results_df[(results_df['function'] == func) & (results_df['mode'] == mode) & (results_df['dim'] == dim) & (results_df['threshold'] == threshold)]
+
+                    if subset.empty:
+                        continue
+
+                    avg_fitnesses = json.loads(subset.iloc[0]['avg_fitnesses'])
+                    f_trace_value = subset.iloc[0]['f_trace']
+                    if isinstance(f_trace_value, str):  
+                        f_trace = json.loads(f_trace_value)  
+                    else:  
+                        f_trace = [f_trace_value]
+
+                    result_vars_value = subset.iloc[0]['result_vars']
+                    if isinstance(result_vars_value, str):  
+                        result_vars = json.loads(result_vars_value)  
+                    else:  
+                        result_vars = [result_vars_value]
+
+                    dim = subset.iloc[0]['dim']
+                    filename = f"../plots/threshold/{func}_{dim}_{mode}.png"
+
+                    ax1 = axes[i, 0]
+                    ax1.plot(range(len(avg_fitnesses)), avg_fitnesses, label=f"{mode} - avg fitness")
+                    ax1.set_yscale('log')
+                    ax1.set_title(f"{func} - {mode} (avg fitness) - threshold {threshold}")
+                    ax1.set_xlabel("Iteration")
+                    ax1.set_ylabel("Avg Fitness (log scale)")
+                    ax1.legend()
+
+                    min_fitness = min(avg_fitnesses)
+                    min_fitness_index = avg_fitnesses.index(min_fitness)
+
+                    ax1.text(min_fitness_index, min_fitness, f"Min: {min_fitness:.2e}", color='blue', fontsize=10, ha='center', va='bottom')
+
+                    ax2 = axes[i, 1]
+                    ax2.plot(range(len(f_trace)), f_trace, label=f"{mode} - f_trace", color='r')
+                    ax2.set_title(f"{func} - {mode} (f_trace)")
+                    ax2.set_xlabel("Iteration")
+                    ax2.set_ylabel("F")
+                    ax2.legend()
+
+                    ax3 = axes[i, 2]
+                    ax3.plot(range(len(result_vars)), result_vars, label=f"{mode} - variance", color='g')
+                    ax3.set_title(f"{func} - {mode} (variance)")
+                    ax3.set_xlabel("Iteration")
+                    ax3.set_ylabel("Variance")
+                    ax3.legend()
+
+                plt.tight_layout()
+                plt.savefig(filename)
