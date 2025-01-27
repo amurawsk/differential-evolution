c1faaa9a1b887075946f44eedfc2b21261b21b94
Adrian Murawski <adrianmurawski79@gmail.com>
2025-01-27 22:24:45 +0100

    changed main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from experiments import *
 
 
+# run all experiments
 def main():
     demonstration()
     different_amplifier_reductor_msr()
     different_target_ratio()
     different_threshold_percentage()
+    comparison()
 
 
 if __name__ == "__main__":
